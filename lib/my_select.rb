eebf71d1602b3de801782bf55f1a2f2efbbf5cfc
Natasha Chernis <natashachernis@gmail.com>
2019-02-21 05:06:40 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,11 +2,7 @@ def my_select(collection)
  i = 0
  selected_collection = []
  while i < collection.length
-   if yield(collection[i]) == true
-     selected_collection << collection[i]
-   else
-     nil
-   end
+   selected_collection << collection[i] if yield(collection[i]) == true
    i += 1
  end
  selected_collection
